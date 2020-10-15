6d3a57d20adcf13831c75a154c168a68d1e9e517
yangwb <425873681@qq.com>
2020-10-15 22:34:01 +0800

    consul

---
+++ userservice/src/main/java/com/yang/userservice/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.yang.userservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "userService:" + port;
+    }
+
+}
---
+++ userview/src/test/java/com/yang/userview/UserViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.userview;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
